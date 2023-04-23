8143d6ff7f91d079e25d67d49fbe276fda593ff7
Margaret300 <margaretnjoki30@gmail.com>
2023-04-23 07:48:20 -0700

    Task 7

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -9,7 +9,7 @@ char a;
 int b;
 long int c;
 long long d;
-flot f;
+float f;
 printf("Size of a char: %d byte(s)\n", sizeof(char));
 printf("Size of an int: %d byte(s)\n", sizeof(int));
 printf("Size of a long int: %d byte(s)\n", sizeof(long int));
