c4ef5326aa0e6b33667268578368535a8ba41394
Margaret300 <margaretnjoki30@gmail.com>
2023-05-02 14:09:26 -0700

    main.h

--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -1,11 +1,10 @@
-#ifndef "MAIN.H"
-
-#define "MAIN.H"
+#ifndef MAIN.H
+#define MAIN.H
 
 #include <stdio.h>
 #include <unlstd.h>
 
-int _putchar(char c);
+int-putchar(char c);
 void reset_to_98(int *n);
 void swap_int(int *a, int *b);
 int _strlen(char *s);
