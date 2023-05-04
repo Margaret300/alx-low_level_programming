c4d80cffd974a2d2d9e2fcc0b379bbfcb01e26dc
Margaret300 <margaretnjoki30@gmail.com>
2023-05-04 13:16:23 -0700

    putchar.c

--- 0x06-pointers_arrays_strings/putchar.c
+++ 0x06-pointers_arrays_strings/putchar.c
@@ -1,12 +1,12 @@
-#incude <unistd.h>
+#include < unistd.h >
 /**
- * putchar -writes the character c stdout
+ * putchar - Writes the character c stdout
  * @c: The character to print
  *
- * Return: On success 1
+ * Return: success 1
  * On error, -1 is returned, and errno is set appropriately
  */
-int _putchar(char c)
+int _putchar(char c);
 {
 	return (write(1, &c, 1))
 }
