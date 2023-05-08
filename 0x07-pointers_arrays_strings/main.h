de014ec4a17bc67d26dc2559abf55dd15d33080e
Margaret300 <margaretnjoki30@gmail.com>
2023-05-08 13:17:41 -0700

    task 0

--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -1,9 +1,5 @@
 #ifndef MAIN_H
 #define MAIN_H
-/**
-*This header file has all prototypes for 0x07-pointers_arrays_strings
-*/
-Int_putchar(char c)
 char *_memset(char *s, char b, unsigned int n);
 char *_memcpy(char *dest, char *src, unsigned int n);
 char *_strchr(char *s, char c);
