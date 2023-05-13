e7ed05145b9e215cbbe321efe7dbec445b1a3c7d
Margaret300 <margaretnjoki30@gmail.com>
2023-05-12 17:14:27 -0700

    task 4

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -3,7 +3,7 @@
 #include <ctype.h>
 #include <string.h>
 /**
- * check_num - check - string there are digit
+ * check_num - To check digit in a string
  * @str: array str
  *
  * Return: Always 0 (Success)
@@ -28,7 +28,7 @@ int check_num(char *str)
 }
 
 /**
- * main - Print the name of the program
+ * main - It print the name of the program
  * @argc: Count arguments
  * @argv: Arguments
  *
