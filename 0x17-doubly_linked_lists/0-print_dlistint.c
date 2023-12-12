5ccd2863f0cd3be76b891a95fa84a21a03ed43e8
margaret300 <margaretnjoki30@gmail.com>
2023-12-12 08:04:05 -0800

    print_dlistint commit

--- 0x17-doubly_linked_lists/0-print_dlistint.c
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -1,11 +1,11 @@
 #include "lists.h"
 
 /**
- * print_dlistint - prints all the elements of a
+ * print_dlistint-prints all the elements of a
  * dlistint_t list
  *
  * @h: head of the list
- * Return: the number of nodes
+ * Return:the number of nodes
  */
 size_t print_dlistint(const dlistint_t *h)
 {
